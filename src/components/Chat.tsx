fc3744db55cb73a9e7cf786a28372acd23ea9bf9
Jack Dewinter <jackdewinter1@gmail.com>
2020-03-19 23:17:51 +0000

    Message being typed appears on screen

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,18 +1,23 @@
 import * as actions from "../store/chat/actions";
 import { RootState } from "../store/index";
 import { connect } from "react-redux";
-import React from "react";
+import React, { useState } from "react";
 
 function Chat(props: any) {
+  const [message, setMessage] = useState("");
+
   const messageList = () =>
     props.messages.map((message: any) => <li>{message.message}</li>);
+
+  const handleChange = (e: any) => setMessage(e.currentTarget.value);
   return (
     <div>
       <h1>Chat</h1>
       <form>
-        <input type="text" />
+        <input type="text" onChange={handleChange} />
         <input type="submit" value="Send" />
       </form>
+      <p>{message}</p>
       <div>{messageList()}</div>
     </div>
   );
