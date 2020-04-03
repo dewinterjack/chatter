b55145d635d797afd0f8613f62f1dbffbc659e8b
Jack Dewinter <Jack.Dewinter@red-gate.com>
2020-04-03 15:39:14 +0100

    Do not duplicate message for sender

--- src/store/chat/reducer.ts
+++ src/store/chat/reducer.ts
@@ -10,6 +10,10 @@ const initialState: ChatState = {
   messages: []
 };
 
+function onlyUnique(value: any, index: any, self: any) {
+  return self.indexOf(value) === index;
+}
+
 export function chatReducer(
   state = initialState,
   action: ChatActionTypes
@@ -19,7 +23,15 @@ export function chatReducer(
       const sorted = [...state.messages, action.payload].sort(
         (a, b) => a.timestamp - b.timestamp
       );
-      return { messages: sorted };
+
+      const uniqueMessages = sorted.filter((thing, i, arr: any) => {
+        return (
+          arr.indexOf(arr.find((t: any) => t.timestamp === thing.timestamp)) ===
+          i
+        );
+      });
+
+      return { messages: uniqueMessages };
 
     case MESSAGE_SENT:
       return {
