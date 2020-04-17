88224a7264c191e652afd340ff7732ee80e292ce
Jack Dewinter <jack.dewinter@red-gate.com>
2020-04-17 13:31:45 +0100

    Remove comments from server
    This is really just to test the github action that was only listening to server paths

--- Chatter.Api/Startup.cs
+++ Chatter.Api/Startup.cs
@@ -16,7 +16,6 @@ namespace Chatter.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors(options => options.AddPolicy("CorsPolicy", builder =>
@@ -30,7 +29,6 @@ namespace Chatter.Api
             services.AddSignalR().AddAzureSignalR();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
